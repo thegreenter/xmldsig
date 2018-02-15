2df4e4d69f068beab195cf7aa82ab316d265f86e
giansalex <giansalex@gmail.com>
2018-02-15 18:02:39 -0500

    simple test X509Certficate

--- tests/export.php
+++ tests/export.php
@@ -1,14 +1,18 @@
 <?php
 
-use Greenter\XMLSecLibs\Tool\PfxConverter;
+use Greenter\XMLSecLibs\Certificate\X509Certificate;
+use Greenter\XMLSecLibs\Certificate\X509ContentType;
 
 require __DIR__.'/../vendor/autoload.php';
 
-$pfx = file_get_contents(__DIR__.'/SFSCert.pfx');
+$pfx = __DIR__.'/SFSCert.pfx';
 $password = '123456';
 
-$converter = new PfxConverter($pfx, $password);
-$pem = $converter
-    ->toPem()
-    ->getResult();
-//file_put_contents('my.pem', $pem);
+try {
+    $certificate = X509Certificate::createFromFile($pfx, $password);
+    echo $certificate->getName();
+    $pem = $certificate->export(X509ContentType::PEM);
+    //file_put_contents('my.pem', $pem);
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
